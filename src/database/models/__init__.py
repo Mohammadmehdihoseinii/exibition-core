eb9f134f6f52a18226b1c066a81605d98a419b37
mohamadmahdihossini <mhdy63403@gmail.com>
2025-12-06 00:04:40 +0330

    ابدیت  مدل کمپانی

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -1,7 +1,18 @@
 from .user import User
 from .profiles import UserProfile, OrganizerProfile, UserPreferredCategory, UserSocialLink
-from .company import CompanyProfile, CompanyDocument
-from .exhibition import Exhibition, ExhibitionTag, ExhibitionMedia, ExpoCompany,VerificationDocument
+from .company import (
+    CompanyProfile,
+    CompanyDocument,
+    CompanyWebsite,
+    CompanyAddress,
+    CompanyPhone,
+    CompanyTag,
+    CompanyVideo,
+    CompanyBrochure,
+    CompanyKnowledgeFile
+)
+
+from .exhibition import Exhibition, ExhibitionTag, ExhibitionMedia, ExpoCompany, VerificationDocument
 from .product import Product, ProductImage
 from .misc import UserFavorite, UserView
 from .enums import (
@@ -11,30 +22,49 @@ from .enums import (
 from .token import Token
 from .tracking import TrackingSession, TrackingPageView
 
+
 __all__ = [
     "User",
     "UserProfile",
     "OrganizerProfile",
     "UserPreferredCategory",
     "UserSocialLink",
+
+    # Company models
     "CompanyProfile",
     "CompanyDocument",
+    "CompanyWebsite",
+    "CompanyAddress",
+    "CompanyPhone",
+    "CompanyTag",
+    "CompanyVideo",
+    "CompanyBrochure",
+    "CompanyKnowledgeFile",
+
+    # Exhibition
     "Exhibition",
     "ExhibitionTag",
     "ExhibitionMedia",
     "ExpoCompany",
+
+    # Product
     "Product",
     "ProductImage",
+
+    # Misc
     "UserFavorite",
     "UserView",
     "Token",
     "TrackingSession",
     "TrackingPageView",
+    
+    # Enums
     "RoleEnum",
     "ApprovalStatusEnum",
     "ExpoStatusEnum",
     "VipLevelEnum",
     "FavoriteTypeEnum",
     "ViewTargetEnum",
+
     "VerificationDocument",
 ]
